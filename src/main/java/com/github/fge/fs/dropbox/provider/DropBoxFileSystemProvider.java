7e85413b2824ae7f0bea6afd894b8e61f4c6ccf3
umjammer <umjammer@gmail.com>
2019-06-20 23:00:07 +0900

    make it work as spi

--- src/main/java/com/github/fge/fs/dropbox/provider/DropBoxFileSystemProvider.java
+++ src/main/java/com/github/fge/fs/dropbox/provider/DropBoxFileSystemProvider.java
@@ -1,13 +1,12 @@
 package com.github.fge.fs.dropbox.provider;
 
 import com.github.fge.filesystem.provider.FileSystemProviderBase;
-import com.github.fge.filesystem.provider.FileSystemRepository;
 
 public final class DropBoxFileSystemProvider
     extends FileSystemProviderBase
 {
-    public DropBoxFileSystemProvider(final FileSystemRepository repository)
+    public DropBoxFileSystemProvider()
     {
-        super(repository);
+        super(new DropBoxFileSystemRepository());
     }
 }
