5b90618b12bd7a7eaa23bc46a77b6002b3bb15e6
umjammer <umjammer@gmail.com>
2020-04-29 17:42:01 +0900

    move codes into vavi-nio-file

--- src/main/java/com/github/fge/fs/dropbox/driver/DropBoxFileSystemDriver.java
+++ src/main/java/com/github/fge/fs/dropbox/driver/DropBoxFileSystemDriver.java
@@ -22,7 +22,6 @@ import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.spi.FileSystemProvider;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -176,7 +175,7 @@ Debug.println("newOutputStream: " + e.getMessage());
         throws IOException
     {
         try {
-            return Util.newDirectoryStream(getDirectoryEntries(dir, filter));
+            return Util.newDirectoryStream(getDirectoryEntries(dir), filter);
         } catch (DbxException e) {
             throw new DropBoxIOException("dir: " + dir, e);
         }
@@ -357,7 +356,7 @@ System.out.println("SeekableByteChannelForWriting::close: scpecial: " + path);
     }
 
     /** */
-    private List<Path> getDirectoryEntries(Path dir,final DirectoryStream.Filter<? super Path> filter) throws IOException, DbxException {
+    private List<Path> getDirectoryEntries(Path dir) throws IOException, DbxException {
         final Metadata entry = cache.getEntry(dir);
 
         if (!isFolder(entry)) {
@@ -378,19 +377,6 @@ System.out.println("SeekableByteChannelForWriting::close: scpecial: " + path);
                 cache.putFile(childPath, child);
             }
             cache.putFolder(dir, list);
-        }
-        
-        //Filtering out the file for which the pattern doesn't match
-        if(filter != null)
-        {
-        	Iterator<Path> iterator = list.iterator();
-        	while (iterator.hasNext()) {
-        		Path filePath = iterator.next();
-        		
-        		if(!filter.accept(filePath)) {
-        			iterator.remove();
-        		}
-        	}
         }
 
         return list;
